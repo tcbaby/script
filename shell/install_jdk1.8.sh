70809a09193321ec41b096327a15beed753fd403
tcbaby <cbtan@foxmail.com>
2020-12-25 14:20:08 +0800

    tomat debug mode

--- shell/install_jdk1.8.sh
+++ shell/install_jdk1.8.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+# author: tcbaby
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"  http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
 

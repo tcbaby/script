9805ef3b50688222ea91308b426e481f770b7cc1
tcbaby <cbtan@foxmail.com>
2021-01-24 18:22:53 +0800

    create ap

---
+++ shell/create_ap.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+# author: tcbaby
+
+sudo pacman -S create_ap
+
+# 1. 直接开启
+sudo create_ap wlp0s20f3 wlp0s20f3 mk qwertyui0p
+
+# 2. 配置服务
+# 修改 /etc/create_ap.conf 下面配置
+if false; then
+    WIFI_IFACE=wlp0s20f3
+    INTERNET_IFACE=wlp0s20f3
+    SSID=mk
+    PASSPHRASE=qwertyui0p
+fi
+
+systemctl start create_ap

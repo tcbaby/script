e2dc4126cd8d63f823a4de2750d32fccdbadbce5
tcbaby <cbtan@foxmail.com>
2021-06-21 10:27:32 +0800

    kde moe theme

---
+++ shell/kde_moe.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# latte
+# Moe Layout(latte): https://www.pling.com/p/1373008
+git clone https://github.com/psifidotos/Latte-Dock.git
+cd Latte-Dock && sh install.sh
+
+# Borderless Maximized Windows
+# https://qastack.cn/ubuntu/253337/remove-title-bar-and-borders-on-maximized-windows-in-kubuntu
+# https://github.com/psifidotos/Latte-Dock/wiki/Tips-&-Tricks#q-can-i-have-different-borderlessmaximized-windows-option-between-different-layouts
+
+
+# https://www.bilibili.com/video/BV1bz4y1z7oV
+Global Theme: Moe
+Plasma Style: Moe
+Kvantum: Moe
+Winows Decorations: Moe、Fluent-light、ChromeOS-light
+Color: Moe
+Icons: McMuse-circle-red
+Cursors: McMojave Cursors 、 Oxygen Yellow Cursors
+dock: latte-dock
+Latte Dock Layout: Moe
+Konsole Theme: Moe
+widgets: System Tray, Application Luncher, Window Buttons, Window Title, Global Menu, Event Calendar, NetspeedWidget, separator, sidebarbutton
+Other Plugin:
+https://github.com/zzag/kwin-effects-yet-another-magic-lamp.git
+https://github.com/psifidotos/applet-window-buttons.git
+https://github.com/tcorreabr/Parachute.git
+https://github.com/wsdfhjxc/virtual-desktop-bar.git
